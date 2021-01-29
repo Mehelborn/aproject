6a3249b25fc8455055486caa2a1a49e31b42dcaf
Mehelborn <33414237+Mehelborn@users.noreply.github.com>
2021-01-29 12:42:15 +0700

    29.01.2021

---
+++ main_code.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+class main_code {
+    public static void main(String args[]) throws IOException {
+        Scanner readfromfile = new Scanner(new File(""))
+    }
+}
