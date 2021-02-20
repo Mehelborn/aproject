0f652b4d1adf4dee0f4d356385f251a3bcd2933b
Mehelborn <33414237+Mehelborn@users.noreply.github.com>
2021-02-20 20:46:10 +0700

    20.02.2021

---
+++ src/aproject/calculate.java
@@ -0,0 +1,21 @@
+package aproject;
+
+public class calculate {
+	
+	public static int summation(int a, int b) {		
+		return a + b;
+	}
+	
+	public static int subtraction(int a, int b) {		
+		return a - b;
+	}
+	
+	public static int division(int a, int b) {		
+		return a / b;
+	}
+	
+	public static int multiplication(int a, int b) {		
+		return a * b;
+	}
+
+}
--- src/aproject/source_code.java
+++ src/aproject/source_code.java
@@ -1,23 +1,62 @@
 package aproject;
 
 import java.util.Scanner;
-import java.io.IOException;
-import java.io.File;
+import static java.lang.System.out;
 
 public class source_code {
 
-	public static void main(String[] args) throws IOException {
-		Scanner sourceFile = new Scanner(new File("data//expressions.txt"));
-		int number1 = sourceFile.nextInt();		
-		String token = sourceFile.next();
-		int result = 0;
-		int number2 = sourceFile.nextInt();
-		if (token.equals("+")) result = number1 + number2;
-		else if (token.equals("-")) result = number1 - number2;
-		else if (token.equals("*")) result = number1 * number2;
+	public static void main(String[] args) {
+		int theFirstNumber, theSecondNumber, theResult;
+		int option;
+		Scanner selector, theFirstEnter, theSecondEnter;
 		
-		System.out.println(number1 + " - " + number2 + " = " + result);
 		
+		
+			// theNumbers
+			
+			out.print("Please enter the first number: ");
+			theFirstEnter = new Scanner(System.in);
+			theFirstNumber = theFirstEnter.nextInt();
+			out.println();
+			out.print("Please enter the second number: ");
+			theSecondEnter = new Scanner(System.in);
+			theSecondNumber = theSecondEnter.nextInt();
+			out.println();
+		
+			// menu
+		
+			out.println("What do you want to do with these numbers?");
+			out.printf("1.Summmation\n2.Subtraction\n3.Multiplication\n"
+				+ "4.Division\n0.Exit\n");
+			out.print("Please enter the number of an option: ");
+			selector = new Scanner(System.in);
+			option = selector.nextInt();
+			out.println();			
+		
+		
+		switch (option) {
+			case 1: 
+				theResult = calculate.summation(theFirstNumber, theSecondNumber);
+				out.printf("%d + %d = %d", theFirstNumber, theSecondNumber, theResult);
+				break;
+			case 2:
+				theResult = calculate.subtraction(theFirstNumber, theSecondNumber);
+				out.printf("%d - %d = %d", theFirstNumber, theSecondNumber, theResult);
+				break;
+			case 3:
+				theResult = calculate.multiplication(theFirstNumber, theSecondNumber);
+				out.printf("%d * %d = %d", theFirstNumber, theSecondNumber, theResult);
+				break;
+			case 4:
+				theResult = calculate.division(theFirstNumber, theSecondNumber);
+				out.printf("%d / %d = %d", theFirstNumber, theSecondNumber, theResult);
+				break;
+			case 0:
+				break;
+			default: 
+				out.println("There is no such option!"); 
+				break;
+		}
 	}
 
 }
